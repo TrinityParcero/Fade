d21635e1cd505240bcc60d0799bf34bb4a92d96e
gxt8566 <gxt8566@IMM-7>
2017-04-03 19:56:54 -0400

    made more code change to grunt class

--- Fade/Fade/Grunt.cs
+++ Fade/Fade/Grunt.cs
@@ -9,68 +9,101 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Fade
 {
+    //grunt states
+    enum GruntState
+    {
+        WalkLeft,
+        WalkRight,
+        JumpLeft,
+        JumpRight
+
+    }
     class Grunt : Enemy
     {
+       
+      
         //one health
         //same speed as player
-        //int gMaxHeight (the largest height the grunt will ever get to)
-        //int ground
-        //bool isSpawn
-        public Grunt(Texture2D asset, Rectangle loc, int speed, double hp, double dmg) : base(asset,loc,speed,hp,dmg)
+        int gMaxHeight; // the largest height the grunt will ever reach
+        int ground; //the grunt as well as the lowest possible height the grunt will reach
+        bool isSpawn;// states whether or not the grunt has spawned
+        int gruntJI; // amount by which the grunt's jump height increments
+        public Rectangle location;// rectangle for the grunt's location
+        public Texture2D sprite { get; set; }
+        public bool jumping { get; set; }//bool to test for grunt jumping
+        public bool falling { get; set; }//bool to test for grunt falling
+        public GruntState gruntState = GruntState.WalkLeft;//initialized grunt state
+        public GruntState prevGruntState;
+
+
+
+        //note: when spawning the grunt make sure to pass in rectangle of player
+        //constructor for grunt class
+        public Grunt(Texture2D asset, Rectangle loc, int speed, double hp, double dmg, Rectangle grd) : base(asset,loc,speed,hp,dmg)
         {
-          //isSpawn = false;
-          //gMaxHeight = 1/2 of playerHeight
-          //ground = the same ground as the one the player starts at
+            isSpawn = false;
+            gMaxHeight = grd.Height / 2;
+            gruntJI = 1;
+            location = loc;
+            ground = location.Y;
+            sprite = asset;
+            jumping = false;
+            falling = false;
 
 
         }
-
-        ///public void move()
-        ///{
-        /// if(isSpawn == false)
-        /// {
-        ///   isSpawn = true;
-        ///   
-        /// }
-        /// 
-        ///}
-        ///
-        /// 
-        /// public void moveUpdate()
-        /// {
-        ///    if(isSpawn == true)
-        ///    {
-        ///    gruntLocation.Y -= gruntJumpIncrement
-        ///    
-        ///    }
-        ///    if(gruntLoction.Y less than or equal to max grunt height)
-        ///    {
-        ///    bool for grunt jump = false
-        ///    bool for grunt fall = true
-        ///    }
-        ///    if(bool for grunt fall == true)
-        ///    {
-        ///    gruntLocation.Y += gruntJumpIncrement
-        ///    }
-        ///    if(gruntLocation.Y >= ground value)
-        ///    {
-        ///    bool for grunt jump = false
-        ///    bool for grunt fall = false
-        ///    }
-        ///    
-        ///    if(playerLocation.X less than or equal to gruntLocation.X)
-        ///    {
-        ///    gruntState = gruntRight
-        ///    gruntLocation.X -= whatever distance grunt travels
-        ///    }
-        ///    else if(playerLocation.X >= gruntLocation.X)
-        ///    {
-        ///    gruntState = grnutLeft
-        ///    gruntLocation.X += whatever distance the grunt travels
-        ///    }
-        /// 
-        /// }
-        ///
+        /// <summary>
+        /// the grunt starts to move as soon as it spawns
+        /// </summary>
+        public void move1()
+        {
+            if(isSpawn == false)
+            {
+            isSpawn = true;
+          
+            }
+         
+        }
+        
+        /// <summary>
+        /// once the grunt is moving, it should move like this
+        /// </summary>
+                 
+         public void move(Player p)
+         {
+            if(isSpawn == true)
+            {
+                location.Y -= gruntJI;
+                jumping = true;
+            }
+            if(location.Y <= gMaxHeight)
+            {
+                jumping = false;
+                falling = true;
+            }
+            if(falling == true)
+            {
+                location.Y += gruntJI;
+            }
+            if(location.Y >= ground)
+            {
+                jumping = false;
+                falling = false;
+            }
+            
+            if(p.location.X <= location.X)
+            {
+                gruntState = GruntState.WalkRight;
+                location.X -= Speed;
+            }
+            else if(p.location.X >= location.X)
+            {
+                gruntState = GruntState.WalkLeft;
+                location.X += Speed;
+            }
+         
+         }
+        
 
 
     }
